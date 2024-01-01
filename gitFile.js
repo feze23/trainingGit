917f6476c433c9db73b48c38f92dc10f348e8bd7
Steve Feze <stevefeze@outlook.com>
2024-01-01 15:04:55 +0100

    html and js file have been updated

--- gitFile.js
+++ gitFile.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $("button").click(function (){
        $("h1").fadeToggle(); 
     })
+    $(".btn").click(function (){
+        $(".hello").html("<h1>another update</h1>")
+    })
 })
